db4d01c7a3b3216b6aa79e8674b13c5394ca75be
ruskstoic <145134275+ruskstoic@users.noreply.github.com>
2023-09-21 11:47:56 +0800

    Update recyclables_deployment.py

--- recyclables_deployment.py
+++ recyclables_deployment.py
@@ -10,9 +10,12 @@ Original file is located at
 # Import
 import streamlit as st
 import subprocess
-if "tensorflow" not in st.sys_installed_packages():
+import pkgutil
+
+if not pkgutil.find_loader("tensorflow"):
     st.warning("Installing TensorFlow...")
     subprocess.call(['pip', 'install', 'tensorflow'])
+
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
