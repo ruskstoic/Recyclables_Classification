40047549ba315599ff938f002e9af14cd6340ec8
ruskstoic <145134275+ruskstoic@users.noreply.github.com>
2023-09-21 15:11:33 +0800

    Update recyclables_deployment.py

--- recyclables_deployment.py
+++ recyclables_deployment.py
@@ -26,7 +26,7 @@ if uploaded_image is not None:
     if st.button('Predict'):
         if uploaded_image:
             # Define the GitHub repository URL and model file path
-            github_repo_url = 'https://github.com/ruskstoic/Recyclables_Classification/raw/main'
+            github_repo_url = 'https://github.com/ruskstoic/Recyclables_Classification/raw/master/Downloads'
             model_filename = 'best_model_checkpoint.h5'
             
             # Download the model file from GitHub
