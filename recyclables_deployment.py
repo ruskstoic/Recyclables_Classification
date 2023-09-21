8b9730f2d743ed6e1635c49a45f417bdc0b30e54
ruskstoic <145134275+ruskstoic@users.noreply.github.com>
2023-09-21 11:42:09 +0800

    Update recyclables_deployment.py
    changes

--- recyclables_deployment.py
+++ recyclables_deployment.py
@@ -9,16 +9,17 @@ Original file is located at
 
 # Import
 import streamlit as st
-!pip install tensorflow==2.5
-import subprocess
-subprocess.call(['pip', 'install', 'tensorflow'])
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 from PIL import Image
 
+if "tensorflow" not in st.sys_installed_packages():
+    st.warning("Installing TensorFlow...")
+    subprocess.call(['pip', 'install', 'tensorflow'])
+
 # Load Model
-filename = 'trained_model.h5'
+filename = 'best_model_checkpoint.h5'
 model = tf.keras.load_model(f'/content/drive/MyDrive/Garbage Classification/Saved_Models/{filename}')
 
 # Preprocess and Predict
