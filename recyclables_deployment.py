7306cdec7a7acd6636ef36c5229862e9f3476d6c
ruskstoic <145134275+ruskstoic@users.noreply.github.com>
2024-04-05 22:09:59 +0800

    Update recyclables_deployment.py
    Removed if condition for first block

--- recyclables_deployment.py
+++ recyclables_deployment.py
@@ -215,28 +215,28 @@ if user_name:
     st.write(img, glass_percent, metal_percent, paper_percent, paper_percent)
 
     #Logging user information
-    if isinstance(img, np.ndarray) and glass_percent != 'NIL' and metal_percent != 'NIL' and paper_percent != 'NIL' and plastic_percent != 'NIL':
-        user_log_filename = 'user_log.txt'
-        log_entry_df = log_user_info(user_name=user_name, user_id=user_id, formatted_datetime_entered=formatted_datetime_entered, tab_id=tab_id,
-                                     img=img, glass_percent=glass_percent, metal_percent=metal_percent, paper_percent=paper_percent, plastic_percent=plastic_percent)
+    # if isinstance(img, np.ndarray) and glass_percent != 'NIL' and metal_percent != 'NIL' and paper_percent != 'NIL' and plastic_percent != 'NIL':
+    user_log_filename = 'user_log.txt'
+    log_entry_df = log_user_info(user_name=user_name, user_id=user_id, formatted_datetime_entered=formatted_datetime_entered, tab_id=tab_id,
+                                 img=img, glass_percent=glass_percent, metal_percent=metal_percent, paper_percent=paper_percent, plastic_percent=plastic_percent)
+
+    #Create Google Sheet Connection Object
+    conn = st.connection('gsheets', type=GSheetsConnection)
     
-        #Create Google Sheet Connection Object
-        conn = st.connection('gsheets', type=GSheetsConnection)
-        
-        # Read existing data from the worksheet
-        existing_data = conn.read(worksheet='Sheet1', usecols=[0,1,2,3,4,5,6,7,8], end='A')
-        
-        # Convert the existing data to a DataFrame (assuming it's already in tabular format)
-        existing_df = pd.DataFrame(existing_data, columns=['Name', 'User_ID', 'Datetime_Entered', 'Tab_ID', 'Image', 'Glass %', 'Metal %', 'Paper %', 'Plastic %'])
-        
-        # Concatenate the existing DataFrame with the new entry DataFrame
-        combined_df = pd.concat([existing_df, log_entry_df], ignore_index=True)
-        
-        # Write the combined DataFrame back to the worksheet
-        conn.update(worksheet='Sheet1', data=combined_df)
-        
-        # Clear cache and display success message
-        st.cache_data.clear()
+    # Read existing data from the worksheet
+    existing_data = conn.read(worksheet='Sheet1', usecols=[0,1,2,3,4,5,6,7,8], end='A')
+    
+    # Convert the existing data to a DataFrame (assuming it's already in tabular format)
+    existing_df = pd.DataFrame(existing_data, columns=['Name', 'User_ID', 'Datetime_Entered', 'Tab_ID', 'Image', 'Glass %', 'Metal %', 'Paper %', 'Plastic %'])
+    
+    # Concatenate the existing DataFrame with the new entry DataFrame
+    combined_df = pd.concat([existing_df, log_entry_df], ignore_index=True)
+    
+    # Write the combined DataFrame back to the worksheet
+    conn.update(worksheet='Sheet1', data=combined_df)
+    
+    # Clear cache and display success message
+    st.cache_data.clear()
     
     #Merge and display user info
     user_info_headers = f'Name: {user_name} | User ID: {user_id} | Date Entered: {formatted_datetime_entered} | Tab ID: {tab_id}'
