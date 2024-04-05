e812a548826c1c77c9407ca3d22d0c8bb25b017c
ruskstoic <145134275+ruskstoic@users.noreply.github.com>
2024-04-05 22:41:16 +0800

    Update recyclables_deployment.py
    Added if condition (if 'user_info_logged' not in st.session_state:) before if user name for first block to run once only

--- recyclables_deployment.py
+++ recyclables_deployment.py
@@ -187,133 +187,134 @@ st.subheader("Model Disclaimer: Work in Progress 🚧\n\nOur model is in its ear
 Please note that it's a beginner model, and while it shows promising results, it is not perfect. We appreciate your understanding as we strive to enhance its performance over time.")
 
 
-#Prompt user to enter their name
-user_name = st.text_input('Hi! What is your name?')
-
-if user_name:
-    #Say Hi
-    st.write(f'Hello {user_name}!')
+## Prompt user to enter their name
+if 'user_info_logged' not in st.session_state:
+    user_name = st.text_input('Hi! What is your name?')
     
-    #Get or create a unique user ID for current session
-    user_id = cookies["user_id"]
-
-    #Get or create a unique tab ID for current session
-    tab_id = get_or_create_tab_ID()
-
-    #Create datetime and format it for log entry
-    datetime_format = '%Y-%m-%d %H:%M:%S'
-    converted_timezone = pytz.timezone('Asia/Singapore')
-    converted_datetime_entered = datetime.now(converted_timezone)
-    formatted_datetime_entered = converted_datetime_entered.strftime(datetime_format)
-
-    # Get the previous values of img and material percentages from session state
-    img = st.session_state.get('img', 'NIL')
-    glass_percent = st.session_state.get('glass_percent', 'NIL')
-    metal_percent = st.session_state.get('metal_percent', 'NIL')
-    paper_percent = st.session_state.get('paper_percent', 'NIL')
-    plastic_percent = st.session_state.get('plastic_percent', 'NIL')
-    st.write(img, glass_percent, metal_percent, paper_percent, paper_percent)
-
-    #Logging user information
-    # if isinstance(img, np.ndarray) and glass_percent != 'NIL' and metal_percent != 'NIL' and paper_percent != 'NIL' and plastic_percent != 'NIL':
-    user_log_filename = 'user_log.txt'
-    log_entry_df = log_user_info(user_name=user_name, user_id=user_id, formatted_datetime_entered=formatted_datetime_entered, tab_id=tab_id,
-                                 img=img, glass_percent=glass_percent, metal_percent=metal_percent, paper_percent=paper_percent, plastic_percent=plastic_percent)
-
-    #Create Google Sheet Connection Object
-    conn = st.connection('gsheets', type=GSheetsConnection)
-    
-    # Read existing data from the worksheet
-    existing_data = conn.read(worksheet='Sheet1', usecols=[0,1,2,3,4,5,6,7,8], end='A')
-    
-    # Convert the existing data to a DataFrame (assuming it's already in tabular format)
-    existing_df = pd.DataFrame(existing_data, columns=['Name', 'User_ID', 'Datetime_Entered', 'Tab_ID', 'Image', 'Glass %', 'Metal %', 'Paper %', 'Plastic %'])
-    
-    # Concatenate the existing DataFrame with the new entry DataFrame
-    combined_df = pd.concat([existing_df, log_entry_df], ignore_index=True)
-    
-    # Write the combined DataFrame back to the worksheet
-    conn.update(worksheet='Sheet1', data=combined_df)
-    
-    # Clear cache and display success message
-    st.cache_data.clear()
-    
-    #Merge and display user info
-    user_info_headers = f'Name: {user_name} | User ID: {user_id} | Date Entered: {formatted_datetime_entered} | Tab ID: {tab_id}'
-    user_info = f'{user_name}_{user_id}_{formatted_datetime_entered}_{tab_id}'
-    uploaded_image = st.file_uploader("Upload your image...", type=['jpeg','jpg','png'])
-    
-    if uploaded_image is not None:
-        st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
-    
-        if st.button('Predict'):
-            if uploaded_image:
-                # Define the GitHub repository URL and model file path
-                github_repo_url = 'https://github.com/ruskstoic/Recyclables_Classification/raw/master/Downloads'
-                model_filename = 'best_model_checkpoint.h5'
-                
-                # Download the model file from GitHub
-                model_url = f'{github_repo_url}/{model_filename}'
-                # response = requests.get(model_url)
-                
-                try:
-                    response = requests.get(model_url)
-                    response.raise_for_status()  # Check for HTTP errors
-                    with open('downloaded_model.h5', 'wb') as model_file:
-                        model_file.write(response.content)
-                    st.success('Model file downloaded successfully!')
-                except requests.exceptions.RequestException as e:
-                    st.error(f'Failed to download the model file: {str(e)}')
-                
-                # Check if the download was successful
-                if response.status_code == 200:
-                    # Save the model file locally
-                    with open(model_filename, 'wb') as f:
-                        f.write(response.content)
-    
-                    # Load the model
-                    import tensorflow as tf
-                    from tensorflow.keras.preprocessing.image import load_img, img_to_array
-                    model = tf.keras.models.load_model(model_filename)
-    
-                    # Preprocess the uploaded image
-                    img = Image.open(uploaded_image)
-                    if img.mode == 'RGBA':
-                        img = img.convert('RGB')
-                    img = img.resize((224, 224))
-                    img = np.array(img) / 255.0
-                    img = np.expand_dims(img, axis=0)
-    
-                    # Make predictions
-                    predictions = model.predict(img)
-                    class_names = ['Glass', 'Metal', 'Paper', 'Plastic']
-                    confidence = format(np.max(predictions) * 100, ".2f")
-                    likely_class = class_names[np.argmax(predictions)]
-    
-                    st.write('Prediction:')
-                    st.write(f'Class: {likely_class}')
-                    st.write(f'Confidence: {confidence}%')
-                
-                    # Save Img and Material Confidence Intervals to Google Sheet
-                    # glass_percent, metal_percent, paper_percent, plastic_percent = predictions[0][0], predictions[0][1], predictions[0][2], predictions[0][3] 
+    if user_name:
+        #Say Hi
+        st.write(f'Hello {user_name}!')
         
-                    # #Create dictionary of all user info
-                    # log_entry_df_predictions = log_user_info(user_name=user_name, user_id=user_id, formatted_datetime_entered=formatted_datetime_entered, tab_id=tab_id,
-                    #              img=img, glass_percent=glass_percent, metal_percent=metal_percent, paper_percent=paper_percent, plastic_percent=plastic_percent)
-                    # #Create Google Sheet Connection Object
-                    # conn = st.connection('gsheets', type=GSheetsConnection)
-                    # # Read existing data from the worksheet
-                    # existing_data = conn.read(worksheet='Sheet1', usecols=[0,1,2,3,4,5,6,7,8], end='A')
-                    # # Convert the existing data to a DataFrame (assuming it's already in tabular format)
-                    # existing_df = pd.DataFrame(existing_data, columns=['Name', 'User_ID', 'Datetime_Entered', 'Tab_ID', 'Image', 'Glass %', 'Metal %', 'Paper %', 'Plastic %'])
-                    # # Concatenate the existing DataFrame with the new entry DataFrame
-                    # combined_df = pd.concat([existing_df, log_entry_df_predictions], ignore_index=True)
-                    # # Write the combined DataFrame back to the worksheet
-                    # conn.update(worksheet='Sheet1', data=combined_df)
-                    # # Clear cache and display success message
-                    # st.cache_data.clear()
-                else:
-                    st.write('Failed to download the model file from GitHub.')
+        #Get or create a unique user ID for current session
+        user_id = cookies["user_id"]
+    
+        #Get or create a unique tab ID for current session
+        tab_id = get_or_create_tab_ID()
+    
+        #Create datetime and format it for log entry
+        datetime_format = '%Y-%m-%d %H:%M:%S'
+        converted_timezone = pytz.timezone('Asia/Singapore')
+        converted_datetime_entered = datetime.now(converted_timezone)
+        formatted_datetime_entered = converted_datetime_entered.strftime(datetime_format)
+    
+        # Get the previous values of img and material percentages from session state
+        img = st.session_state.get('img', 'NIL')
+        glass_percent = st.session_state.get('glass_percent', 'NIL')
+        metal_percent = st.session_state.get('metal_percent', 'NIL')
+        paper_percent = st.session_state.get('paper_percent', 'NIL')
+        plastic_percent = st.session_state.get('plastic_percent', 'NIL')
+        st.write(img, glass_percent, metal_percent, paper_percent, paper_percent)
+    
+        ## Logging user information
+        user_log_filename = 'user_log.txt'
+        log_entry_df = log_user_info(user_name=user_name, user_id=user_id, formatted_datetime_entered=formatted_datetime_entered, tab_id=tab_id,
+                                     img=img, glass_percent=glass_percent, metal_percent=metal_percent, paper_percent=paper_percent, plastic_percent=plastic_percent)
+    
+        # Create Google Sheet Connection Object
+        conn = st.connection('gsheets', type=GSheetsConnection)
+        
+        # Read existing data from the worksheet
+        existing_data = conn.read(worksheet='Sheet1', usecols=[0,1,2,3,4,5,6,7,8], end='A')
+        
+        # Convert the existing data to a DataFrame (assuming it's already in tabular format)
+        existing_df = pd.DataFrame(existing_data, columns=['Name', 'User_ID', 'Datetime_Entered', 'Tab_ID', 'Image', 'Glass %', 'Metal %', 'Paper %', 'Plastic %'])
+        
+        # Concatenate the existing DataFrame with the new entry DataFrame
+        combined_df = pd.concat([existing_df, log_entry_df], ignore_index=True)
+        
+        # Write the combined DataFrame back to the worksheet
+        conn.update(worksheet='Sheet1', data=combined_df)
+        
+        # Clear cache and display success message
+        st.cache_data.clear()
+        
+        #Merge and display user info
+        user_info_headers = f'Name: {user_name} | User ID: {user_id} | Date Entered: {formatted_datetime_entered} | Tab ID: {tab_id}'
+        user_info = f'{user_name}_{user_id}_{formatted_datetime_entered}_{tab_id}'
+        uploaded_image = st.file_uploader("Upload your image...", type=['jpeg','jpg','png'])
+
+        ##Upload Image
+        if uploaded_image is not None:
+            st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
+        
+            if st.button('Predict'):
+                if uploaded_image:
+                    # Define the GitHub repository URL and model file path
+                    github_repo_url = 'https://github.com/ruskstoic/Recyclables_Classification/raw/master/Downloads'
+                    model_filename = 'best_model_checkpoint.h5'
+                    
+                    # Download the model file from GitHub
+                    model_url = f'{github_repo_url}/{model_filename}'
+                    # response = requests.get(model_url)
+                    
+                    try:
+                        response = requests.get(model_url)
+                        response.raise_for_status()  # Check for HTTP errors
+                        with open('downloaded_model.h5', 'wb') as model_file:
+                            model_file.write(response.content)
+                        st.success('Model file downloaded successfully!')
+                    except requests.exceptions.RequestException as e:
+                        st.error(f'Failed to download the model file: {str(e)}')
+                    
+                    # Check if the download was successful
+                    if response.status_code == 200:
+                        # Save the model file locally
+                        with open(model_filename, 'wb') as f:
+                            f.write(response.content)
+        
+                        # Load the model
+                        import tensorflow as tf
+                        from tensorflow.keras.preprocessing.image import load_img, img_to_array
+                        model = tf.keras.models.load_model(model_filename)
+        
+                        # Preprocess the uploaded image
+                        img = Image.open(uploaded_image)
+                        if img.mode == 'RGBA':
+                            img = img.convert('RGB')
+                        img = img.resize((224, 224))
+                        img = np.array(img) / 255.0
+                        img = np.expand_dims(img, axis=0)
+        
+                        # Make predictions
+                        predictions = model.predict(img)
+                        class_names = ['Glass', 'Metal', 'Paper', 'Plastic']
+                        confidence = format(np.max(predictions) * 100, ".2f")
+                        likely_class = class_names[np.argmax(predictions)]
+        
+                        st.write('Prediction:')
+                        st.write(f'Class: {likely_class}')
+                        st.write(f'Confidence: {confidence}%')
+                    
+                        # Save Img and Material Confidence Intervals to Google Sheet
+                        # glass_percent, metal_percent, paper_percent, plastic_percent = predictions[0][0], predictions[0][1], predictions[0][2], predictions[0][3] 
+            
+                        # #Create dictionary of all user info
+                        # log_entry_df_predictions = log_user_info(user_name=user_name, user_id=user_id, formatted_datetime_entered=formatted_datetime_entered, tab_id=tab_id,
+                        #              img=img, glass_percent=glass_percent, metal_percent=metal_percent, paper_percent=paper_percent, plastic_percent=plastic_percent)
+                        # #Create Google Sheet Connection Object
+                        # conn = st.connection('gsheets', type=GSheetsConnection)
+                        # # Read existing data from the worksheet
+                        # existing_data = conn.read(worksheet='Sheet1', usecols=[0,1,2,3,4,5,6,7,8], end='A')
+                        # # Convert the existing data to a DataFrame (assuming it's already in tabular format)
+                        # existing_df = pd.DataFrame(existing_data, columns=['Name', 'User_ID', 'Datetime_Entered', 'Tab_ID', 'Image', 'Glass %', 'Metal %', 'Paper %', 'Plastic %'])
+                        # # Concatenate the existing DataFrame with the new entry DataFrame
+                        # combined_df = pd.concat([existing_df, log_entry_df_predictions], ignore_index=True)
+                        # # Write the combined DataFrame back to the worksheet
+                        # conn.update(worksheet='Sheet1', data=combined_df)
+                        # # Clear cache and display success message
+                        # st.cache_data.clear()
+                    else:
+                        st.write('Failed to download the model file from GitHub.')
 
 #Streamlit Tracker End
 streamlit_analytics.stop_tracking(unsafe_password="test123")
