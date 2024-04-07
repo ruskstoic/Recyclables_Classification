6df8c67096d1f821de43a0ffa6f3a6f6a14bbdcc
ruskstoic <145134275+ruskstoic@users.noreply.github.com>
2024-04-07 16:03:20 +0800

    Update recyclables_deployment.py
    imported tempfile

--- recyclables_deployment.py
+++ recyclables_deployment.py
@@ -38,6 +38,8 @@ from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+import tempfile
+
 
 ## Streamlit Tracker Start
 streamlit_analytics.start_tracking()
