2a7f81a6ae244977d3bfa397163856312030870e
ruskstoic <145134275+ruskstoic@users.noreply.github.com>
2024-04-07 21:08:30 +0800

    Update recyclables_deployment.py
    Added to convert st.secret format to JSON-formatted string

--- recyclables_deployment.py
+++ recyclables_deployment.py
@@ -203,8 +203,10 @@ SCOPES = ['https://www.googleapis.com/auth/drive',
 # def authenticate():
 # gdrive_auth_secret = os.environ.get('GDRIVE_AUTHENTICATION_CREDENTIALS')
 gdrive_auth_secret = st.secrets['GDRIVE_AUTHENTICATION_CREDENTIALS']
+gdrive_auth_secret_json = json.dumps(gdrive_auth_secret)
+credentials_dict = json.loads(gdrive_auth_secret_json)
 st.write('gdrive_auth_secret', gdrive_auth_secret)
-credentials_dict = json.loads(gdrive_auth_secret)
+st.write(gdrive_auth_secret_json)
 st.write(credentials_dict)
 # creds = service_account.Credentials.from_service_account_file(credentials_dict, scopes=SCOPES)
     # return creds
