29facf2a8584eae58103444723ae2c0582028315
ruskstoic <145134275+ruskstoic@users.noreply.github.com>
2024-04-07 21:31:56 +0800

    Update recyclables_deployment.py
    Try authenticate function

--- recyclables_deployment.py
+++ recyclables_deployment.py
@@ -201,15 +201,13 @@ SCOPES = ['https://www.googleapis.com/auth/drive',
 ]
 
 # Function to authenticate with Google Drive API
-# def authenticate():
-# gdrive_auth_secret = os.environ.get('GDRIVE_AUTHENTICATION_CREDENTIALS')
-gdrive_auth_secret = st.secrets['GDRIVE_AUTHENTICATION_CREDENTIALS']
-st.write('gdrive_auth_secret', gdrive_auth_secret)
-
-st.write("client_email", gdrive_auth_secret["client_email"])
-st.write("private_key", gdrive_auth_secret["private_key"])
-# creds = service_account.Credentials.from_service_account_file(credentials_dict, scopes=SCOPES)
-    # return creds
+def authenticate():
+    gdrive_auth_secret = st.secrets['GDRIVE_AUTHENTICATION_CREDENTIALS']
+    # st.write('gdrive_auth_secret', gdrive_auth_secret)
+    # st.write("client_email", gdrive_auth_secret["client_email"])
+    # st.write("private_key", gdrive_auth_secret["private_key"])
+    creds = service_account.Credentials.from_service_account_file(gdrive_auth_secret, scopes=SCOPES)
+    return creds
 
 # Authenticate with Google Drive API
 # credentials = authenticate()
