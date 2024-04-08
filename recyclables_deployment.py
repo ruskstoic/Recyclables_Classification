bfca111c9b1ce671f9772e12ecae86eff793f8bb
ruskstoic <145134275+ruskstoic@users.noreply.github.com>
2024-04-08 23:38:47 +0800

    Update recyclables_deployment.py
    Added debug statement

--- recyclables_deployment.py
+++ recyclables_deployment.py
@@ -215,6 +215,7 @@ model_folder_contents = {}
 # Download the model folder as a zip file
 for file in files:
     request = service.files().get_media(fileId=file['id'])
+    st.write(request)
     fh = io.BytesIO()
     downloader = MediaIoBaseDownload(fh, request)
     done = False
